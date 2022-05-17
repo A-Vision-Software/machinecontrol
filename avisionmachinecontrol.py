d77e8b9dbb76cfbc8d2f5b8d97d52382eef5870d
Arnold Velzel <info@a-vision.software>
2022-05-17 10:25:02 +0200

    Initial version

---
+++ avisionmachinecontrol.py
@@ -0,0 +1,237 @@
+#/*******************************************************************************
+#*
+#* File description :       A-Vision Solutions machine control card module
+#*
+#* Created by       :       Arnold Velzel
+#* Created on       :       04-04-2022
+#*
+#* Required         :       pcf8575, gpiozero
+#*
+#* sudo apt update
+#* sudo apt upgrade
+#* sudo apt autoremove
+#* sudo apt-get install libffi-dev
+#* sudo apt-get install python3-pip
+#* sudo pip3 install smbus2
+#* sudo pip3 install w1thermsensor (https://github.com/timofurrer/w1thermsensor)
+#*
+#* sudo raspi-config
+#*  -> Interface Options
+#*    -> Enable I2C support
+#*    -> Enable 1-Wire support
+#*
+#*******************************************************************************/
+
+from ast import Try
+from gpiozero import Button, LED
+from signal import pause
+from time import sleep
+
+from pcf8575 import PCF8575
+from w1thermsensor import W1ThermSensor
+
+# Constants for easy programming
+class machineconstants:
+    OFF = 0
+    ON = 1
+    LEFT = 1
+    RIGHT = 2
+    UP = 1
+    DOWN = 2
+
+# All power outputs as a list
+class machinecontrolpower(list):
+
+    def __init__(self, machinecontrol, *args, **kwargs):
+        super(machinecontrolpower, self).__init__(*args, **kwargs)
+        self.machinecontrol = machinecontrol
+
+    def __setitem__(self, key, value):
+        assert key <= len(self)
+        self.machinecontrol._set_power_output(key, value)
+
+    def __getitem__(self, key):
+        assert key <= len(self)
+        return self.machinecontrol._P[key]
+
+    def __len__(self):
+        return 2
+
+# All additional (PCF) inputs/outputs as a list
+class machinecontrolport(list):
+
+    def __init__(self, machinecontrol, *args, **kwargs):
+        super(machinecontrolport, self).__init__(*args, **kwargs)
+        self.machinecontrol = machinecontrol
+
+    def __setitem__(self, key, value):
+        assert key < len(self)
+        self.machinecontrol._set_output(key, value)
+
+    def __getitem__(self, key):
+        assert key < len(self)
+        return self.machinecontrol._get_input(key)
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+    def __len__(self):
+        return 32
+
+# All standard motor control outputs as a list
+class machinecontrolmotor(list):
+
+    def __init__(self, machinecontrol, *args, **kwargs):
+        super(machinecontrolmotor, self).__init__(*args, **kwargs)
+        self.machinecontrol = machinecontrol
+
+    def __setitem__(self, key, value):
+        assert key <= len(self)
+        self.machinecontrol._set_motor_output(key, value)
+
+    def __getitem__(self, key):
+        assert key <= len(self)
+        return self.machinecontrol._OUT[key]
+
+    def __len__(self):
+        return 3
+
+# All two-way motor control outputs as a list
+class machinecontroltwowaymotor(list):
+
+    def __init__(self, machinecontrol, *args, **kwargs):
+        super(machinecontroltwowaymotor, self).__init__(*args, **kwargs)
+        self.machinecontrol = machinecontrol
+
+    def __setitem__(self, key, value):
+        assert key <= len(self)
+        self.machinecontrol._set_twowaymotor_output(key, value)
+
+    def __getitem__(self, key):
+        assert key <= len(self)
+        return self.machinecontrol._M[key]
+
+    def __len__(self):
+        return 7
+
+# A single two-way motor control class
+class twowaymotor:
+
+    def __init__(self, pinLeft, pinRight):
+        self.left = LED(pinLeft)
+        self.right = LED(pinRight)
+        self._direction = machineconstants.OFF
+    
+    @property
+    def direction(self):
+        return None
+    
+    @direction.setter
+    def direction(self, value):
+        if (value == machineconstants.OFF):
+            self.left.off()
+            self.right.off()
+
+        if (value == machineconstants.LEFT):
+            if (self._direction != machineconstants.OFF):
+                self.left.off()
+                self.right.off()
+                sleep(0.1)
+
+            self.left.on()
+
+        if (value == machineconstants.RIGHT):
+            if (self._direction != machineconstants.OFF):
+                self.left.off()
+                self.right.off()
+                sleep(0.01)
+
+            self.right.on()
+
+        self._direction = value
+
+# The machine control class
+class machinecontrol(machineconstants):
+
+    def __init__(self):
+        self._PCF1 = PCF8575(1, 0x20)
+        self._PCF2 = PCF8575(1, 0x21)
+
+        self._OUT = [None, LED(13), LED(12), LED(6)]
+        self._M = [None, twowaymotor(11, 8), twowaymotor(25, 10), twowaymotor(16, 26), twowaymotor(23, 24), twowaymotor(19, 18), twowaymotor(9, 7), twowaymotor(22, 5)]
+        self._P = [None, LED(27), LED(17)]
+
+    @property
+    def temperature(self):
+        try:
+            DS18B20 = W1ThermSensor()
+            return DS18B20.get_temperature()
+        except:
+            return None
+    
+    @property
+    def power(self):
+        return machinecontrolpower(self)
+
+    @property
+    def output(self):
+        return machinecontrolport(self)
+
+    @property
+    def input(self):
+        return machinecontrolport(self)
+
+    @property
+    def motor(self):
+        return machinecontrolmotor(self)
+
+    @property
+    def twowaymotor(self):
+        return machinecontroltwowaymotor(self)
+
+    def _set_power_output(self, num, value):
+        if (value == self.ON):
+            value = True
+        if (value == self.OFF):
+            value = False
+        if (1 <= num <= 2):
+            if (value):
+                self._P[num].on()
+            else:
+                self._P[num].off()
+
+    def _set_output(self, num, value):
+        if (value == self.ON):
+            value = True
+        if (value == self.OFF):
+            value = False
+        if (num <= 15):
+            self._PCF1.port[num] = value
+        if (num > 15):
+            self._PCF2.port[num - 16] = value
+
+    def _get_input(self, num):
+        if (num <= 15):
+            self._PCF1.port[num] = True # initialise as INPUT
+            return self._PCF1.port[num]
+        if (num > 15):
+            self._PCF2.port[num - 16] = True # initialise as INPUT
+            return self._PCF2.port[num - 16]
+        return None
+
+    def _set_motor_output(self, num, value):
+        if (value == self.ON):
+            value = True
+        if (value == self.OFF):
+            value = False
+        if (1 <= num <= 3):
+            if (value):
+                self._OUT[num].on()
+            else:
+                self._OUT[num].off()
+
+    def _set_twowaymotor_output(self, num, value):
+        if (1 <= num <= 7):
+            self._M[num].direction = value
+
---
+++ examples.py
@@ -0,0 +1,110 @@
+#/*******************************************************************************
+#*
+#* (c) 2022 Copyright A-Vision Software
+#*
+#* File description :        Python machine control examples
+#*
+#* Created by       :        Arnold Velzel
+#* Created on       :        17-05-2022
+#*
+#*******************************************************************************/
+
+from time import sleep
+from avisionmachinecontrol import machinecontrol
+
+# Initialise the machine control card
+machine = machinecontrol()
+
+#############################
+### TWO-WAY MOTOR OUTPUTS ###
+#############################
+#
+# Valid output values are:
+#   OFF, LEFT, RIGHT, UP, DOWN
+#
+#############################
+print('TWO-WAY MOTOR OUTPUTS')
+# Assign a machine two-way motor output to a variable (range 1..7)
+nicemotorname = machine.twowaymotor[4]
+# Set a two-way motor output direction (using variable)
+nicemotorname.direction = machinecontrol.UP
+# Set a two-way motor output direction (directly)
+machine.twowaymotor[3] = machinecontrol.RIGHT
+
+sleep(1)
+
+############################
+### POWER (220V) OUTPUTS ###
+############################
+#
+# Valid output values are: ON, OFF
+#
+############################
+print('POWER (220V) OUTPUTS')
+# Assign a power output to a variable (range 1..2)
+nicepowername = machine.power[2]
+# Set a power output (using variable)
+nicepowername.value = machinecontrol.ON
+# Set a power output (directly)
+machine.power[1] = machinecontrol.ON
+# Valid values are: ON, OFF
+
+sleep(1)
+
+##############################
+### STANDARD MOTOR OUTPUTS ###
+##############################
+#
+# Valid output values are:
+#   ON, OFF
+#
+##############################
+print('STANDARD MOTOR OUTPUTS')
+# Assign a standard motor output to a variable (range 1..3)
+nicemotornamestd = machine.motor[2]
+# Set a standard motor output (using variable)
+nicemotornamestd.value = machinecontrol.ON
+# Set a standard motor output (directly)
+machine.motor[3] = machinecontrol.ON
+
+sleep(1)
+
+#########################################
+### STANDARD DIGITAL INPUTS / OUTPUTS ###
+#########################################
+#
+# INFO
+#  Input pins have a pull-up resistor and
+#  will return True if not connected
+#  All I/O is configured as INPUTS at
+#  system boot
+#
+# !WARNING!
+#  Setting an output to OFF and supplying 
+#  a voltage (VCC) to the output pin will
+#  damage the interface card
+#
+# Valid output values are:
+#   ON, OFF
+#
+#########################################
+print('STANDARD DIGITAL INPUTS / OUTPUTS')
+# Read an input (range 0..31)
+print('I4:', machine.input[4])
+# Set an output (range 0..31)
+machine.output[19] = machinecontrol.OFF
+
+sleep(1)
+
+#########################
+### TEMPERATURE INPUT ###
+#########################
+#
+# Return value in °C
+#
+#########################
+print('TEMPERATURE INPUT')
+# Read temperature (DB18B20 sensor)
+print('T:', machine.temperature, '°C')
+
+sleep(1)
